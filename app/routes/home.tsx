546b090a8635af1ba67b5c594325cccd12b843ce
dhuiPark <hasong925@gmail.com>
2026-01-11 22:52:53 +0900

    init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,9 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+import { Button } from "~/components/ui/button";
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <h1 className="text-5xl">
+      home <Button className="max-w-xl">roasted garlic.</Button>
+    </h1>
+  );
 }
